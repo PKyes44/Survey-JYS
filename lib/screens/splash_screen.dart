043e11f1e3a4fa53210acc38ac8bc88c3b05250d
PKyes44 <rugate23@gmail.com>
2024-03-27 13:27:57 +0900

    20240327-03

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -46,15 +46,13 @@ class _SplashScreenState extends State<SplashScreen> {
       splashTransition: SplashTransition.fadeTransition,
       backgroundColor: Colors.white,
       splashIconSize: Sizes.size36 * 5,
-      nextScreen: 
-      // isToken
-      //     ? MakeQuestionScreen(
-      //         studentNumber: studentNumber,
-      //         name: name,
-      //         point: point,
-      //       )
-          // : 
-          const LoginScreen(),
+      nextScreen: isToken
+          ? MakeQuestionScreen(
+              studentNumber: studentNumber,
+              name: name,
+              point: point,
+            )
+          : const LoginScreen(),
     );
   }
 }
